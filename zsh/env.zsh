5760ca3a19cbdd3302dec1c5f70e68008b2cab1a
Zenlian <zeninglian@gmail.com>
2020-03-17 19:39:54 +0800

    nvim: include as submodule.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,7 +5,8 @@ export EDITOR='nvim'
 
 # fzf
 export FZF_DEFAULT_OPTS='--bind ctrl-e:down,ctrl-u:up --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 export FZF_COMPLETION_TRIGGER='\'
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT='80%'
