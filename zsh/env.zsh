484a32f4730704bebcf97dee5ba26cffe88f80e0
Zenlian <zeninglian@gmail.com>
2020-04-13 00:24:35 +0800

    zsh(fzf-tab): add fzf-tab as custom plugin

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,9 +4,8 @@ export TERM_ITALICS=true
 export EDITOR='nvim'
 
 # fzf
-export FZF_DEFAULT_OPTS='--bind ctrl-e:down,ctrl-u:up --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
-
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_OPTS='--bind=tab:down,btab:up,change:top,space:toggle'
+#export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 export FZF_COMPLETION_TRIGGER='\'
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT='80%'
