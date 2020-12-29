6c18f2a8270be0a91c335638b204c26145e7668a
Zenlian <zeninglian@gmail.com>
2020-12-30 04:37:25 +0800

    zsh(alias): more aliase for exa

--- zsh/.config/zsh/snippets/aliases.zsh
+++ zsh/.config/zsh/snippets/aliases.zsh
@@ -9,14 +9,16 @@ alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
 
-command -v exa > /dev/null && alias ls='exa'
 command -v exa > /dev/null
 if [ $? -eq 0 ]; then
     alias ls='exa'
-    alias l='exa -alh'
-    unalias lsa
-    alias ll='exa -lh'
     alias la='exa -a'
+    alias l='exa -al'
+    alias lsa='exa -al'
+    alias ll='exa -l'
+    alias lt='exa -T'
+    alias lta='exa -Ta'
+    alias llta='exa -lTa'
 fi
 
 command -v trash > /dev/null && alias rm='trash'
