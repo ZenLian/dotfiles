5dd5d014cb509857d8c592d432a8997360332891
ZenLian <zeninglian@gmail.com>
2023-02-13 22:14:59 +0800

    feat(chezmoi): add awesome

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
+log_info() {
+    fmt="$1"
+    shift
+    printf "\x1b[32m==>\x1b[0m $fmt\n" "$@"
+}
+
 # ensure correct working directory
 cd "$(dirname "$0")" || exit 1
 CWD="$(pwd)"
-printf 'source dir: %s\n' "$CWD"
+log_info 'CWD: %s' "$CWD"
 
 # ensure chezmoi is installed
-version=$(chezmoi --version)
-$! || (echo 'chezmoi: not installed' && exit 1)
-printf '%s\n' "$version"
+CHEZMOI='chezmoi'
+if ! version=$($CHEZMOI --version 2> /dev/null) ; then
+    log_info 'installing chezmoi into %s' "$HOME/.local/bin"
+    # printf '%s\n' "installing chezmoi into $HOME/.local/bin"
+    BINDIR="$HOME/.local/bin" sh -c "$(curl -fsLS get.chezmoi.io)"
+    CHEZMOI="$BINDIR/chezmoi"
+else
+    # printf '%s\n' "$version"
+    log_info "$version"
+fi
 
 # chezmoi apply
